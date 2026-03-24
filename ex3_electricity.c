c7c9a29c8f1d04565916188e63b7e6752d1a8e9c
Rugaba Benedict <rugababenedict@gmail.com>
2026-03-24 20:34:33 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,23 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: RUGABA BENEDICT*/
+/* Student Number: 25/U/BIE/01417/PE*/
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float units_consumed;
+    float cost_per_unit;
+    float total_bill;
+
+    printf("Enter units consumed: ");
+    scanf("%f", &units_consumed);
+
+    printf("Enter cost per unit: ");
+    scanf("%f", &cost_per_unit);
+
+    total_bill = (units_consumed + cost_per_unit);
+
+    printf("Total electricity bill:%.2f", total_bill);
 
     return 0;
 }
