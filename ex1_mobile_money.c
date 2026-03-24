c3c2bb3516c221c8ee3fb29e65de7d7801797103
Rugaba Benedict <rugababenedict@gmail.com>
2026-03-24 20:07:18 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,29 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: RUGABA BENEDICT*/
+/* Student Number: 25/U/BIE/01417/PE */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float amount;
+    float trans;
+    float transaction;
+    float amount_deducted;
+
+    printf ("Enter amount to be sent: ");
+    scanf("%f", &amount);
+
+    printf("Enter transaction fee: %");
+    scanf("%f", &trans);
+
+    transaction = amount * (trans/100.00);
+    amount_deducted = (amount + transaction);
+
+    printf("Transaction fee:%.2f \n", transaction);
+    printf("Amount deducted:%.2f", amount_deducted);
+    
+    
+
 
     return 0;
 }
